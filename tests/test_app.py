031aef1684eb6a3c587d1fa4d9a10a3b37bfd3b1
Zarif Atai (MZ) <Zarif.Atai@achmea.nl>
2023-04-05 17:58:31 +0200

    Adjusted tests in accordance with changes in MyObject class

--- tests/test_app.py
+++ tests/test_app.py
@@ -5,12 +5,12 @@ from src.mypkg.app import MyObject
 
 @pytest.fixture
 def app_fixture() -> MyObject:
-    return MyObject()
+    return MyObject(5.0, 5.0, 5.0)
 
 
 def test_area(app_fixture: MyObject) -> None:
-    assert app_fixture.calculate_area(5.0, 5.0) == 25
+    assert app_fixture.calculate_area() == 25
 
 
 def test_volume(app_fixture: MyObject) -> None:
-    assert app_fixture.calculate_volume(5.0, 5.0, 5.0) == 125
+    assert app_fixture.calculate_volume() == 125
